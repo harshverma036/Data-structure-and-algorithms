dc746b3d872c658458ec8ed691f9f032be343da1
harshverma036 <harshverma0362@gmail.com>
2022-08-14 12:55:08 +0530

    selection sort done

---
+++ sorting/selectionSort.js
@@ -0,0 +1,19 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    var lowest = i;
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+
+    if (i !== lowest) {
+      var temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+  return arr;
+}
+
+console.log(selectionSort([12, 2, 54, 33, 2, 44, 89, 0, 90]));
